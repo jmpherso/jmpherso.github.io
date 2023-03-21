cff691901528fbf73b4dcbd7029c4cbb86a70458
Jerry MacPherson <jmpherso@gmail.com>
2023-03-21 15:08:06 -0500

    fix resume link

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -3,7 +3,7 @@ import './FloatingButton.css';
 
 const FloatingButton = () => {
   return (
-    <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/jmpherso/jmpherso.github.io/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">
       <div className="floatingButton">
         resume
       </div>
